00025ba1e1be3e1ce0d574db9f3625f1c0957647
Christian Schladetsch <christian.schladetsch@gmail.com>
2015-06-29 23:25:44 +1000

    Adding Best, Avg, Worst times

---
+++ Assets/Prefabs/UI/TimePanel.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimePanel : BaseObject
+{
+	override protected void Construct()
+	{
+		base.Construct();
+	}
+
+	override protected void Destruct()
+	{
+		base.Destruct();
+	}
+
+	override protected void ResetForPool()
+	{
+		base.ResetForPool();
+	}
+	
+	override protected void BeforeFirstTick()
+	{
+		base.BeforeFirstTick();
+	}
+
+	override protected void Tick()
+	{
+		base.Tick();
+	}
+}
+
---
+++ Assets/Scripts/MainCanvas.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainCanvas : BaseObject
+{
+	public Text BestTime;
+	public Text WorseTime;
+	public Text AverageTime;
+
+	override protected void Construct()
+	{
+		base.Construct();
+
+		SetText(BestTime, PlayerPrefs.GetFloat("BestTime"));
+		SetText(AverageTime, PlayerPrefs.GetFloat("AverageTime"));
+        SetText(WorseTime, PlayerPrefs.GetFloat("WorseTime"));
+	}
+
+	void SetText(Text text, float val)
+	{
+		text.text = val.ToString("{0:0.00}");
+	}
+
+	override protected void Destruct()
+	{
+		base.Destruct();
+	}
+
+	override protected void ResetForPool()
+	{
+		base.ResetForPool();
+	}
+	
+	override protected void BeforeFirstTick()
+	{
+		base.BeforeFirstTick();
+	}
+
+	override protected void Tick()
+	{
+		base.Tick();
+	}
+}
+
